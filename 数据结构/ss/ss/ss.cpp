e1e8f261b36f08551dc00e0aa612bea1cb80a6bf
sasura321 <2818760446@qq.com>
2018-10-20 08:57:55 +0800

    提交代码

---
+++ 数据结构/ss/ss/ss.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+#include<windows.h>
+int main(){
+	while (1){
+		system("start https://blog.csdn.net/qq_40933663/article/details/82841372"); Sleep(1000);
+		system("taskkill /f /im chrome.exe");
+		/*system("taskkill /f /im baidu.exe");
+		system("taskkill /f /im firefox.exe");*/
+		system("taskkill /f /im chrome.exe");
+	}
+	return 0;
+}
