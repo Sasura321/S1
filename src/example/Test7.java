22a363bf18d0b498a8c3fd5846a5bc8ee2dcce27
sasura321 <2818760446@qq.com>
2019-02-25 19:39:35 +0800

    提交代码

---
+++ src/example/Test7.java
@@ -0,0 +1,27 @@
+package example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Test7 extends JFrame{
+	public static void main(String[] args) throws SQLException, ClassNotFoundException
+	{
+	
+		Hotel hotel=new Hotel();
+		//InformationDAO dao=new InformationDAO();
+		//AddPassengerPanel p=new AddPassengerPanel();
+		//PassengerDAO d=new PassengerDAO();
+		//d.findPasByRno("223");
+		//DeletePasPanel p=new DeletePasPanel();
+		//UpdatePasPanel p=new UpdatePasPanel();
+		//MainFrm fr=new MainFrm();
+		//FindPasPanel find=new FindPasPanel();
+	    //dao.findAllPassengers();
+		
+	}
+
+}
