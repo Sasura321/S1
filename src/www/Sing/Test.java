e5e4b48de40cda0e78bd5ac63b9d295796941376
sasura321 <2818760446@qq.com>
2019-03-03 10:56:46 +0800

    提交代码

---
+++ src/www/Sing/Test.java
@@ -0,0 +1,22 @@
+package www.Sing;
+
+import javax.naming.directory.SearchResult;
+import java.io.*;
+import java.net.ServerSocket;
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner result = new Scanner("666");
+        //PrintStream os = new PrintStream();
+        //PrintWriter re = new PrintWriter(os);
+        try {
+            ServerSocket serverSocket = new ServerSocket();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+
+        }
+        //re.close();
+    }
+}
